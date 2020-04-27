8072075e7e575e8f875900f261a0323af94acf71
meeks21 <anthony.meeks3@gmail.com>
2020-04-27 02:12:00 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+
+
+// asks the first question
+var name = prompt("Let's play the sum game." + "<br>" + "Please enter your name");
+
+
+// asks the second question
+var firstNumber = prompt("Enter first number");
+
+// asks the second question
+var secondNumber = prompt("Enter second number");
+
+var total = parseInt("firstNumber") + parseInt("secondNumber");
+document.write("Hi, " + ("name") + "! The sum of " + ("firstNumber") + " + " + ("secondNumber") + " is " + total); 
